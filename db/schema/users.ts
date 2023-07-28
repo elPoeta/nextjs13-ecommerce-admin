633bb28ec576edf951d5b3c5d3acbbdc3058039c
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-07-28 07:30:54 -0300

    create users table

--- db/schema/users.ts
+++ db/schema/users.ts
@@ -1,5 +1,27 @@
-import { pgTable, serial } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  timestamp,
+  uniqueIndex,
+  varchar,
+  text,
+} from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-});
+export const users = pgTable(
+  "users",
+  {
+    id: serial("id").primaryKey().notNull(),
+    name: varchar("name", { length: 100 }),
+    email: varchar("email", { length: 256 }).notNull(),
+    emailVerified: timestamp("emailVerified"),
+    image: varchar("image", { length: 256 }),
+    role: text("role", { enum: ["admin", "user"] })
+      .notNull()
+      .default("user"),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+    updated_at: timestamp("updated_at").notNull().defaultNow(),
+  },
+  (user) => ({
+    emailIndex: uniqueIndex("users__email__idx").on(user.email),
+  })
+);
